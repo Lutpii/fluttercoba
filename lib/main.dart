b4d2dcb28f81b101a2d850b16001a5c41281b4a8
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2022-10-16 22:30:28 +0700

    tombol kiri aga tengah

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _HomePageState extends State<HomePage> {
               preferredSize: Size.fromHeight(150), child: Text("Hai Semuanya")),
         ),
         floatingActionButton: Padding(
-          padding: const EdgeInsets.only(left: 50),
+          padding: const EdgeInsets.only(left: 250),
           child: Row(children: [
             FloatingActionButton(
               onPressed: () {
